fd8e87f3b15b416bdd0f7c4706cca9be8ae311e2
aidan-lee123 <aidan.lee123@gmail.com>
2020-03-12 17:01:14 +1100

    Added In YarnSpinner
    and plugin Merino for Dialogue System

---
+++ train-detective/Assets/Scripts/NPC/NPCInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInfo : MonoBehaviour
+{
+    public string _characterName = ""; 
+    public LayerMask _player;
+
+    public void Awake(){
+        _player = LayerMask.GetMask("Player");
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
