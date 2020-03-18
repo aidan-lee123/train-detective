838076f7ec8b26c15f0fa00007a6a792e376b88b
aidan-lee123 <aidan.lee123@gmail.com>
2020-03-18 17:26:45 +1100

    Added HSV changer for bacground sprites
    Will allow changing of colours for background sprites, for night time etc

---
+++ train-detective/Assets/Scripts/Environment/BackgroundColour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BackgroundColour : MonoBehaviour
+{
+    public Color spriteColour;
+
+    [Range(0.0f, 1.1f)]
+    public float Hue;
+
+    [Range(0.0f, 1.0f)]
+    public float Saturation;
+
+    [Range(0.0f, 1.0f)]
+    public float Value = 1f;
+
+    private Transform[] children;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        spriteColour = new Color(spriteColour.r, spriteColour.g, spriteColour.b);
+        spriteColour = Color.HSVToRGB( Hue, Saturation, Value);
+
+
+        foreach (Transform t in transform) {
+            children = t.gameObject.GetComponentsInChildren<Transform>();
+            foreach (Transform c in children) {
+                c.GetComponent<SpriteRenderer>().color = spriteColour;
+            }
+        }
+    }
+
+
+
+
+}
